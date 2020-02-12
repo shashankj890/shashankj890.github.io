146388b5b25380c1509e45de4b05c76f65b48b42
shashankj890 <shashankj890@gmail.com>
2020-02-12 13:33:19 +0530

    animate about and work

---
+++ script.js
@@ -0,0 +1,20 @@
+function funcabout(){
+    var x = document.getElementById("about");
+    x.style.opacity="1";
+    x.style.left="35%";
+}
+function funcwork(){
+    var x = document.getElementById("work");
+    x.style.opacity="1";
+    x.style.left="35%";
+}
+function funcback1(){
+    var x = document.getElementById("about");
+    x.style.opacity="1";
+    x.style.left="100%";
+}
+function funcback2(){
+    var x = document.getElementById("work");
+    x.style.opacity="1";
+    x.style.left="100%";
+}
